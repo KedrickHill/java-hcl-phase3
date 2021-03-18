ee03ea4e777efdc8d5cd8b26951090a238ec10bb
KedrickHill <kedrickjhill@gmail.com>
2021-03-18 10:33:21 -0500

    Added new functionality to TestFromController

--- Projects/UserFeedback/src/main/java/com/project/Feedback/controllers/TestFormController.java
+++ Projects/UserFeedback/src/main/java/com/project/Feedback/controllers/TestFormController.java
@@ -1,7 +1,6 @@
 package com.project.Feedback.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.Feedback.entities.Feedback;
 import com.project.Feedback.services.FeedbackService;
 
@@ -30,24 +25,19 @@ public class TestFormController {
 	
 	
 	@PostMapping("/test_form")
-	public String submitTestForm(@RequestBody Feedback fb, ModelMap model) throws JsonMappingException, JsonProcessingException {
-		try {
+	public String submitTestForm(@RequestBody Feedback fb, ModelMap model) {
 			RestTemplate restTemplate = new RestTemplate();
-			String testForm = "http://localhost:8090/feedbacks";
-			ResponseEntity<String> response = restTemplate.getForEntity(testForm + "/7", String.class);
-			ObjectMapper mapper = new ObjectMapper();
-			JsonNode root = mapper.readTree(response.getBody());
-			JsonNode name = root.path("name");
-			model.addAttribute(name);
+//			URL testForm = new URL("http://localhost:8090/feedbacks/{feedback}");
+//			ResponseEntity<String> response = restTemplate.getForEntity(testForm + "/7", String.class);
+//			ObjectMapper mapper = new ObjectMapper();
+//			JsonNode root = mapper.readTree(response.getBody());
+//			JsonNode name = root.path("name");
+//			model.addAttribute(name);
+			String result = restTemplate.getForObject("http://localhost:8090/feedbacks/{feedback}", String.class, 7); 
 			return "testformjsp";
-		} catch (JsonMappingException e) {
-			throw e;
-		} catch (JsonProcessingException e2) {
-			throw e2;
-		}
+	}
 		
-//	TODO: Implement form submissiont
+//	TODO: Implement form submission
 //	TODO: call RestTemplate and make json request to localhost.../feedback
 
-	}
 }
