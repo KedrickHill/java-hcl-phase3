11db697bbbb3eba7f03ee090c2154d1d46486d13
KedrickHill <kedrickjhill@gmail.com>
2021-03-23 13:06:10 -0500

    Init with added functionality to run

---
+++ Projects/SpringSecurity/src/main/java/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public HttpSecurity http() throws Exception {
+		return super.getHttp();
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/","/index").permitAll().anyRequest().authenticated().and()
+		.formLogin().loginPage("/login").permitAll().and()
+		.logout().permitAll();
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user").password("password").roles("USER").build();
+		
+		return new InMemoryUserDetailsManager(user);
+	}
+}
